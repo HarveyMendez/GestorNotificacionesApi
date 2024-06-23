db534cd2d598e6ba6171ddfcd76145965b462f36
HarveyMendez <harvey.mendez@ucr.ac.cr>
2024-06-23 01:35:26 -0600

    add notification mark method

--- Controllers/GestorMensajeriaController.cs
+++ Controllers/GestorMensajeriaController.cs
@@ -17,27 +17,27 @@ namespace GestorNotificacionesApi.Controllers
             _utils = utils;
         }
 
-        [HttpPost]
-        public string EncriptarString(string stringAconvertir) 
+        [HttpPost("/EncriptarTexto")]
+        public string EncriptarString(string stringAconvertir)
         {
             //encriptar
             string encryptedConnectionString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(stringAconvertir));
             return encryptedConnectionString;
         }
 
-        [HttpGet]
-        public IActionResult GestionarNotificaciones() 
+        [HttpGet("/GestionarNotificaciones")]
+        public IActionResult GestionarNotificaciones()
         {
             List<EmailDTO> notificaciones = new List<EmailDTO>();
-            using (SqlConnection conn = _utils.GetConnection()) 
-            { 
+            using (SqlConnection conn = _utils.GetConnection())
+            {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("sp_obtener_notificaciones_pendientes", conn);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 SqlDataReader dr = cmd.ExecuteReader();
-                while(dr.Read()) 
+                while (dr.Read())
                 {
-                    notificaciones.Add(new EmailDTO { Id = (int)dr["IdNotificacion"], Subject = (string)dr["Asunto"], 
+                    notificaciones.Add(new EmailDTO { Id = (int)dr["IdNotificacion"], Subject = (string)dr["Asunto"],
                         Body = (string)dr["Cuerpo"], AddressTo = (string)dr["Correo"] });
                 }
                 conn.Close();
@@ -47,6 +47,43 @@ namespace GestorNotificacionesApi.Controllers
             return Ok();
         }
 
+
+        [HttpPost("/CrearEstudiante")]
+        public IActionResult CrearEstudiante(EstudianteDTO infoEstudiante) 
+        {
+            using (SqlConnection conn = _utils.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("sp_crear_estudiante", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Nombre", infoEstudiante.Nombre);
+                cmd.Parameters.AddWithValue("@Apellido", infoEstudiante.Apellidos);
+                cmd.Parameters.AddWithValue("@Carne", infoEstudiante.Carne);
+                cmd.Parameters.AddWithValue("@Correo", infoEstudiante.Correo);
+                cmd.Parameters.AddWithValue("@Telefono", infoEstudiante.Telefono);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                conn.Close();
+            }
+            return Ok();
+        }
+
+        private void MarcarNotificacionEnviada(int idNotificacion) 
+        {
+            using (SqlConnection conn = _utils.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("sp_marcar_notificacion_enviada", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@IdNotificacion", idNotificacion);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                conn.Close();
+            }
+        }
+
         private async void EnviarCorreos(List<EmailDTO> notificaciones) 
         {
             foreach (EmailDTO notificacion in notificaciones) 
@@ -54,7 +91,7 @@ namespace GestorNotificacionesApi.Controllers
                 using HttpResponseMessage response = await _utils.GetAPIHost().PostAsJsonAsync(_utils.GetEmailAPI(), notificacion);
                 if (response.IsSuccessStatusCode) 
                 {
-                    //ALGO
+                    MarcarNotificacionEnviada(notificacion.Id);
                 }
             }
         }
---
+++ DTO/EstudianteDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace GestorNotificacionesApi.DTO
+{
+    public class EstudianteDTO
+    {
+        public string Nombre { get; set; } 
+
+        public string Apellidos { get; set; }
+
+        public string Carne {  get; set; }
+
+        public string Correo { get; set; }
+
+        public string Telefono { get; set; }
+
+
+    }
+}
